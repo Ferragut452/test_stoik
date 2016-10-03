ac29f7600d01437f3d517cce3f62adec3270d4b1
Ferragut452 <Ferragut@yandex.ua>
2016-10-03 03:35:12 +0300

    all

---
+++ js/custom.js
@@ -0,0 +1,57 @@
+$(document).ready(function(){
+  $('.slider').slick({
+    dots: true,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    arrows: true
+  });
+});
+
+
+// <script>
+// 	 $('#slider').slick({
+// 	  	slidesToShow: 3,
+//   		slidesToScroll: 1,
+//   		centerMode: true,
+//   		infinite:false,
+//   		arrows: true,
+//   		centerPadding: '',
+// 	  	autoplay: true,
+//   		autoplaySpeed: 3000,
+//   		dots: true,
+//   		responsive: [
+//     {
+//       breakpoint: 1024,
+//       settings: {
+//         slidesToShow: 3,
+//         slidesToScroll: 1
+//       }
+//     },
+//       {
+//       breakpoint: 850,
+//       settings: {
+//         slidesToShow: 1,
+//         slidesToScroll: 1,
+//         fade: true
+//       }
+//     }
+//   ]
+// });
+// </script>
+
+// // <script>
+// //     $(function(){
+// //         $('#menu').slicknav({
+// //         	label: ''
+// //         });
+// //     });
+// // </script>
+
+
+
+//   $(document).ready(function(){
+//     $('.slider').slick({
+//       setting-name: setting-value
+//     });
+//   });
